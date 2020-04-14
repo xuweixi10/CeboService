8860f3c6eb2bb18352ae79cb75ecfe7dbe774ce3
HWalkingMan <971813982@qq.com>
2020-04-14 16:36:49 +0800

    fix pom

---
+++ src/test/java/com/nan/cebo/CeboTest.java
@@ -0,0 +1,24 @@
+package com.nan.cebo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Description 测试类
+ * @Author WM
+ * @Date 2020/4/14 16:23
+ */
+@SpringBootTest(classes = Cebo.class)
+@RunWith(SpringRunner.class)
+public class CeboTest {
+
+  @Test
+  public void testArticle() {
+
+  }
+}
